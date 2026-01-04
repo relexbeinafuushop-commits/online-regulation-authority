bcad96b89ceac43271fa073288f5f9e0e8af5cab
relexbeinafuushop-commits <relexbeinafuushop@gmail.com>
2026-01-04 09:41:45 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const menulinks = document.querySelector('.navbar_menu');
 menu.addEventListener('click',function(){
     menu.classList.toggle('is-active');
     menulinks.classList.toggle('active');
+
 });
+const links = menu.quaryselectorALL('a');
+ links.forEach(link =>{links.addEventlistener('click',(e) =>{e.stopPropagation();
+                                                            });
+                      });
